e75fe13f24b4606744c7038308e034d91049a202
jamessorsona <jamessorsona@gmail.com>
2023-04-29 14:18:27 +0800

    Updated README.md

--- python-model/hdc.py
+++ python-model/hdc.py
@@ -1,14 +1,24 @@
 import numpy as np                
 import time
 
+# performs logical AND on two hypervectors
 def distance(A,B):
     return sum(np.logical_and(A,B))
 
+# permute hypervector
 def perm(A,N):
+    """ 
+    A: hypervector
+    N: number of bits to shift
+    """
     return np.roll(A,N)
 
-# 04/18/2023 optimized generation of random hv
+# generate random hypervector
 def u_gen_rand_hv(D,d):
+    """ 
+    D: dimension of hypervector
+    d: density of hypervector
+    """
     if D % 2 != 0:
         raise ValueError("Error - D must be an even number")
     
@@ -18,24 +28,41 @@ def u_gen_rand_hv(D,d):
     
     return rand_hv.astype(int)
 
-# 04/18/2023 optimized generation of item memory
+# create item memory
 def create_item_mem(N,D,d, first=None, last=None):
-    # insert nice code here
+    """
+    N: number of items
+    D: dimension of hypervector
+    d: density of hypervector
+    """
     item_mem = {}
+
     if first is None:
         first = u_gen_rand_hv(D,d)
     if last is None:
         last = u_gen_rand_hv(D,d)
+
     item_mem.update({0:first})
     
     for i in range(1,N-1):
         temp_hv = np.concatenate((last[:int((D/(N-1))*(N-i-1))],first[int((D/(N-1))*(N-i-1)):])) 
         item_mem.update({i:temp_hv})
+
     item_mem.update({N-1:last})
+
     return item_mem
 
-# 04/18/2023 optimized encoding
+# encodes a voice sample
 def hdc_encode(voice, voice_im, D, d, Q=10, t1=0, remove_list=[]):
+    """
+    voice: voice sample
+    voice_im: item memory
+    D: dimension of hypervector
+    d: density of hypervector
+    Q: quantization factor (default: 10)
+    t1: first threshold (default: 0)
+    remove_list: list of indices to remove from bundle (default: [])
+    """
     voice = np.array(voice)
     voice_quantized = np.where(voice < 1, np.floor(voice * (Q / 2)) + (Q / 2), np.floor((voice - 0.0001) * (Q / 2)) + (Q / 2)).astype(int)
     feature_hv_list = np.array([voice_im[x] for x in voice_quantized])
@@ -73,7 +100,6 @@ def dimension_sparsifier(non_bin_reg, dimensionality, sparsity, remove_list):
     return non_bin_reg, remove_list
 
 def similarity_search(voice,voice_im,voice_am,D,d,Q,t1, remove_list):
-    # insert nice code here
     start = time.time()
     sim_score = 0
     sim_letter = '0'
@@ -81,7 +107,6 @@ def similarity_search(voice,voice_im,voice_am,D,d,Q,t1, remove_list):
     encoding_time = time.time() -start
     start = time.time()
     for each in voice_am.items():
-    #print(each)
         similarity = np.sum(np.logical_and(test_hv,each[1]))
         if (similarity>sim_score):
             sim_score,sim_letter = similarity, each[0]
--- python-model/hdc_dimension_sparsifier.py
+++ python-model/hdc_dimension_sparsifier.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# 04/20 dimension sparsifier
 def dimension_sparsifier(bin_reg, remove_idx):
     comp = enumerate(np.where(bin_reg[0]==bin_reg[1], bin_reg[0], "False"))
     remove = [i for i in comp if i[1]!="False"]
@@ -19,3 +18,4 @@ def dimension_sparsifier(bin_reg, remove_idx):
     #delete the specified indices in all of the classes
     for i in range(len(bin_reg)):
         bin_reg[i] = np.delete(bin_reg[i], remove_idx)
+
