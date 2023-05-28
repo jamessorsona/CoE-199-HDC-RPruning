7b3731e5b81cae1bdf10ce475151d8a277bdbb4b
jamessorsona <jamessorsona@gmail.com>
2023-05-28 21:22:18 +0800

    update

--- helper-scripts/synthesis_tcl_maker.py
+++ helper-scripts/synthesis_tcl_maker.py
@@ -7,83 +7,99 @@ Run this file inside the parent project folder: sparse_hdc_rpruning
 Usage: python3 synthesis_automation_maker.py [file.sv]
 """
 
-################### ERROR HANDLING ###################
+def create_tcl_file(tcl_file, filename, raw_filename):
+    with open(tcl_file, "a") as fh:
+        fh.write("set search_path \"$search_path mapped lib cons rtl\"\n")
+        fh.write("set link_library \"* /cad/tools/libraries/dwc_logic_in_gf22fdx_sc7p5t_116cpp_base_csc20l/GF22FDX_SC7P5T_116CPP_BASE_CSC20L_FDK_RELV02R80/model/timing/db/GF22FDX_SC7P5T_116CPP_BASE_CSC20L_TT_0P80V_0P00V_0P00V_0P00V_25C.db\"\n")
 
-if len(sys.argv) < 2:
-	print("ERROR! Missing file name. Please run the script as follows:")
-	print("python3 synthesis_automation_maker.py [file.sv]")
-	sys.exit(1)
+        fh.write(f"read_sverilog {raw_filename} > logs/{filename}/{filename}-read_sverilog.log\n")
 
-if sys.argv[1][-3:] != ".sv":
-	print("ERROR! File must be a .sv file.")
-	sys.exit(1)
+        fh.write(f"current_design {filename}\n")
+        fh.write("link\n")
 
-# obtain .sv file from command line argument
-raw_filename = sys.argv[1]
-filename = raw_filename[:-3]
+        fh.write(f"check_design > logs/{filename}/{filename}-check_design.log\n")
+        # fh.write(f"source timing.con")
 
-# create logs folder if it doesn't exist
-if not os.path.exists("logs"):
-	os.mkdir("logs")
+        fh.write(f"compile > logs/{filename}/{filename}-compile.log\n")
+        
+        fh.write(f"report_constraint -all_violators > logs/{filename}/{filename}-report_constraint.log\n")
+        fh.write(f"report_area > logs/{filename}/{filename}-report_area.log\n")
+        fh.write(f"report_timing > logs/{filename}/{filename}-report_timing.log\n")
+        fh.write(f"report_power > logs/{filename}/{filename}-report_power.log\n")
 
-# create folder for this file if it doesn't exist
-if not os.path.exists(f"logs/{filename}"):
-	os.mkdir(f"logs/{filename}")
-# make a warning if it exists
-else:
-	print(f"WARNING! logs/{filename} folder already exists. Overwriting files...")
-	# remove all files inside the folder
-	for file in os.listdir(f"logs/{filename}"):
-		os.remove(f"logs/{filename}/{file}")
-print(f"Created logs/{filename} folder")
-#####################################################
+        # fh.write(f"write_file -format verilog -hierarchy -output mapped/{filename}/{filename}_mapped.v\n")
+        # fh.write(f"write_file -format ddc -hierarchy -output mapped/{filename}/{filename}_mapped.ddc\n")
 
-tcl_file = f"synthesize_{filename}.tcl"
+        # fh.write(f"write_sdf mapped/{filename}/{filename}_mapped.sdf\n")
+        # fh.write(f"write_sdc mapped/{filename}/{filename}_mapped.sdc\n")
 
-with open(tcl_file, "a") as fh:
+        fh.write("quit\n")
 
-	fh.write("set search_path \"$search_path mapped lib cons rtl\"")
-	fh.write("set link_library */cad/tools/libraries/dwc_logic_in_gf22fdx_sc7p5t_116cpp_base_csc20l/GF22FDX_SC7P5T_116CPP_BASE_CSC20L_FDK_RELV02R80/model/timing/db/GF22FDX_SC7P5T_116CPP_BASE_CSC20L_TT_0P80V_0P00V_0P00V_0P00V_25C.db")
+def execute_tcl_file(tcl_file, filename):
+    print(f"Executing {tcl_file}...")
+    os.system(f"dc_shell -f {tcl_file} -output_log_file logs/{filename}/{filename}-compile.log")
 
-	fh.write(f"read_sverilog {raw_filename} > logs/{filename}/{filename}-read_sverilog.log\n")
 
-	fh.write(f"current_design {filename}\n")
-	fh.write("link\n")
+def main():
+	################### ERROR HANDLING ###################
+    if len(sys.argv) < 2:
+        print("ERROR! Missing file name. Please run the script as follows:")
+        print("python3 synthesis_automation_maker.py [file.sv]")
+        sys.exit(1)
 
-	fh.write(f"check_design > logs/{filename}/{filename}-check_design.log\n")
-	# fh.write(f"source timing.con")
+    if sys.argv[1][-3:] != ".sv":
+        print("ERROR! File must be a .sv file.")
+        sys.exit(1)
 
-	fh.write(f"compile > logs/{filename}/{filename}-compile.log\n")
-	
-	fh.write(f"report_constraint -all_violators > logs/{filename}/{filename}-report_constraint.log\n")
-	fh.write(f"report_area > logs/{filename}/{filename}-report_area.log\n")
-	fh.write(f"report_timing > logs/{filename}/{filename}-report_timing.log\n")
-	fh.write(f"report_power > logs/{filename}/{filename}-report_power.log\n")
+    # obtain .sv file from command line argument
+    raw_filename = sys.argv[1]
+    filename = raw_filename[:-3]
 
-	# fh.write(f"write_file -format sverilog -hierarchy -output mapped/{filename}/{filename}_mapped.sv\n")
-	# fh.write(f"write_file -format ddc -hierarchy -output mapped/{filename}/{filename}_mapped.ddc\n")
+    # create logs folder if it doesn't exist
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
 
-	# fh.write(f"write_sdf mapped/{filename}/{filename}_mapped.sdf\n")
-	# fh.write(f"write_sdc mapped/{filename}/{filename}_mapped.sdc\n")
+    # create folder for this file if it doesn't exist
+    if not os.path.exists(f"logs/{filename}"):
+        os.mkdir(f"logs/{filename}")
+    # make a warning if it exists
+    else:
+        print(f"WARNING! logs/{filename} folder already exists. Overwriting files...")
+        # remove all files inside the folder
+        for file in os.listdir(f"logs/{filename}"):
+            os.remove(f"logs/{filename}/{file}")
+    print(f"Created logs/{filename} folder")
+    #####################################################
 
-	fh.write("quit\n")
+    tcl_file = f"synthesize_{filename}.tcl"
 
-# print success message
-print(f"Success! Created {tcl_file}")
+    # check if tcl file already exists
+    if os.path.exists(tcl_file):
+        print(f"WARNING! {tcl_file} already exists. Overwriting...")
+        os.remove(tcl_file)
+    print(f"Creating {tcl_file}...")
+    # create tcl file
+    create_tcl_file(tcl_file, filename, raw_filename)
+    # print success message
+    print(f"Success! Created {tcl_file}")
 
-# record the execution time
-start_time = time.time()
-print("-----------------------------------------------")
-# execute the tcl file
-print(f"Executing {tcl_file}...")
-os.system(f"dc_shell -f {tcl_file} -output_log_file logs/{filename}/{filename}-compile.log")
-# DONE
-print("-----------------------------------------------")
-print("DONE!")
-# print execution time
-print(f"Execution time: {time.time() - start_time} seconds")
+    # record execution time
+    start_time = time.time()
+    print("-----------------------------------------------")
+    # execute the tcl file
+    execute_tcl_file(tcl_file, filename)
+    print("-----------------------------------------------")
+    print("DONE!")
+    # print execution time
+    print(f"Execution time: {time.time() - start_time} seconds")
 
-# count files inside the logs folder
-logs_count = len(os.listdir(f"logs/{filename}"))
-# print number of files created
-print(f"Created {logs_count} files in logs/{filename} folder")
+    # count files inside the logs folder
+    logs_count = len(os.listdir(f"logs/{filename}"))
+    # print number of files created
+    print(f"Created {logs_count} files in logs/{filename} folder")
+
+
+if __name__ == "__main__":
+	main()  
+
+
